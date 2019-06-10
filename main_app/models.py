07f65484ac1157ff3dd842cf4b4993dab944670d
Aibek <aibekzhv@gmail.com>
2019-06-10 15:18:05 -0700

    create Model Pizza

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.CharField(max_length=250)
+    pizzeria = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
