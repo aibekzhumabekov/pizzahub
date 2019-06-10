a3efece153f1981edf40240865ad2d8b70501436
Aibek <aibekzhv@gmail.com>
2019-06-10 15:21:50 -0700

    rendering views

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,13 @@
 from django.shortcuts import render
+from .models import Pizza
 
-# Create your views here.
-from django.http import HttpResponse
-
-# Add the Pizza class & list and view function below the imports
-class Pizza:
-    def __init__(self, name, ingredients, pizzeria, country):
-        self.name = name
-        self.ingredients = ingredients
-        self.pizzeria = pizzeria
-        self.country = country
-
-pizzas = [
-    Pizza('Rhode Island', 'N/A', 'Figidini', 'USA'),
-    Pizza('Copenhagen', 'N/A', 'Mother', 'Denmark'),
-    Pizza('Di Farra Pizza', 'N/A', 'Di Fara', 'New York, USA')
-    
-]
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 def about(request):
   return render(request, 'about.html')
   # Add new view
 def pizzas_index(request):
+    pizzas = Pizza.objects.all()
     return render(request, 'pizzas/index.html', { 'pizzas': pizzas })
