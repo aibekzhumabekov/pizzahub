e7fc06300d7a973f6f956ec095b6f8dc2c0135c1
Aibek <aibekzhv@gmail.com>
2019-06-12 15:45:43 -0700

    adds Photo model

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Pizza(models.Model):
     name = models.CharField(max_length=100)
-    ingredients = models.CharField(max_length=250)
+    ingredients = models.TextField(max_length=300)
     pizzeria = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pizza_id': self.id})
